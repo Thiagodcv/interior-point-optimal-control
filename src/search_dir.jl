42327a975aa85481b4d3d1d88dfb89a53f7e13a2
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-11-14 14:41:12 -0800

    Did basic implementation for compute_kkt_residual. Now need to test

--- src/search_dir.jl
+++ src/search_dir.jl
@@ -16,17 +16,24 @@ by different functions later.
         - "h"::Array: the linear inequality constraint limits (p,),
         - "A"::Array: the linear equality constraint matrix (m,n),
         - "b"::Array: the linear equality constraint limits (m,).
-- `res_array::Array`: an array which is overwritten, and transformed into the KKT residual.
+- `res_array::Array`: an (n+2p+m,) array which is overwritten, and transformed into the KKT residual.
 - `x::Array`: the current primal iterate (n,).
 - `s::Array`: the current slack variable iterate for the inequality constraint (p,). Strictly positive.
-- `lambda::Array`: the current dual iterate for the inequality constraint (p,).
+- `lambda::Array`: the current dual iterate for the inequality constraint (p,). Strictly positive.
 - `nu::Array`: the current dual iterate for the equality constraint (m,).
 
 # Returns
-- `Array`: the KKT residual.
+- `Array`: the KKT residual. OR MAYBE DON'T RETURN -- JUST MODIFY RES_ARRAY
 """
 function compute_kkt_residual(param_dict, res_array, x, s, lambda, nu)
-    return -1
+    n = size(param_dict["Q"])[1]
+    p = size(param_dict["G"])[1]
+    m = size(param_dict["A"])[1]
+
+    res_array[1:n] = param_dict["Q"] * x + param_dict["q"] + transpose(param_dict["G"]) * lambda + transpose(param_dict["A"]) * nu
+    res_array[n+1:n+p] = Diagonal(s) * lambda
+    res_array[n+p+1:n+2*p] = param_dict["G"] * x + s - param_dict["h"]
+    res_array[n+2*p+1:n+2*p+m] = param_dict["A"] * x - param_dict["b"]
 end
 
 function compute_kkt_Jacobian()
