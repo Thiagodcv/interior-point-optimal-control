030884a944f696337e993d912a1c4682adc7a608
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-11-14 17:12:25 -0800

    Made it so that an old res_mat doesn't have to be passed in by user

--- src/search_dir.jl
+++ src/search_dir.jl
@@ -2,7 +2,7 @@ using LinearAlgebra
 
 
 """
-    compute_kkt_residual(param_dict, res_array, x, lambda, nu, s)
+    compute_kkt_residual(param_dict, x, lambda, nu, s, res_mat)
 
 Computes the residual vector which encodes the KKT conditions of a quadratic program.
 NOTE: does NOT include the centering parameter nor the Mehrotra correction. Those are added on
@@ -16,21 +16,34 @@ by different functions later.
         - "h"::Array: the linear inequality constraint limits (p,),
         - "A"::Array: the linear equality constraint matrix (m,n),
         - "b"::Array: the linear equality constraint limits (m,).
-- `res_array::Array`: an (n+2p+m,) array which is overwritten, and transformed into the KKT residual.
 - `x::Array`: the current primal iterate (n,).
 - `lambda::Array`: the current dual iterate for the inequality constraint (p,). Strictly positive.
 - `nu::Array`: the current dual iterate for the equality constraint (m,).
 - `s::Array`: the current slack variable iterate for the inequality constraint (p,). Strictly positive.
+- `res_mat::Array`: an (n+2p+m,) array which is overwritten, and transformed into the KKT residual.
+
+# Returns
+- `Array`: the residual vector. Only returns if argument for res_mat not specified by user.
 """
-function compute_kkt_residual(param_dict, res_array, x, lambda, nu, s)
+function compute_kkt_residual(param_dict, x, lambda, nu, s, res_mat=nothing)
     n = size(param_dict["Q"])[1]
     p = size(param_dict["G"])[1]
     m = size(param_dict["A"])[1]
 
-    res_array[1:n] = param_dict["Q"] * x + param_dict["q"] + transpose(param_dict["G"]) * lambda + transpose(param_dict["A"]) * nu
-    res_array[n+1:n+p] = Diagonal(s) * lambda
-    res_array[n+p+1:n+2*p] = param_dict["G"] * x + s - param_dict["h"]
-    res_array[n+2*p+1:n+2*p+m] = param_dict["A"] * x - param_dict["b"]
+    new_res_mat = false
+    if isnothing(res_mat)
+        new_res_mat = true
+        res_mat = zeros((n+2*p+m,))
+    end
+
+    res_mat[1:n] = param_dict["Q"] * x + param_dict["q"] + transpose(param_dict["G"]) * lambda + transpose(param_dict["A"]) * nu
+    res_mat[n+1:n+p] = Diagonal(s) * lambda
+    res_mat[n+p+1:n+2*p] = param_dict["G"] * x + s - param_dict["h"]
+    res_mat[n+2*p+1:n+2*p+m] = param_dict["A"] * x - param_dict["b"]
+
+    if new_res_mat
+        return res_mat
+    end
 end
 
 
--- src/test/test_search_dir.jl
+++ src/test/test_search_dir.jl
@@ -4,6 +4,7 @@ using Test
 @testset "test_compute_kkt_residual" begin
     """
     Test for computing the KKT residual for a 2d problem with 1 equality and 4 inequality (box) constraints.
+    Test both cases for when res_mat is specified by user, and when it isn't.
     """
     n = 2
     p = 4
@@ -23,13 +24,16 @@ using Test
     s = [3.; 1.; 3.; 1.]
     
     F = zeros((n + 2*p + m,))
-    compute_kkt_residual(param_dict, F, x, lambda, nu, s)
+    compute_kkt_residual(param_dict, x, lambda, nu, s, F)
+    F_new = compute_kkt_residual(param_dict, x, lambda, nu, s)  # F not passed in.
     true_F = [13/2; -7/2; 3.; 2.; 9.; 4.; 5/2; 1/2; 5/2; 1/2; 0.]
 
     print("F: ", F)
     print("true_F: ", true_F)
+    print("F_new: ", F_new)
     tol = 1e-7
     @test norm(F - true_F) < tol
+    @test norm(F_new - true_F) < tol
 end
 
 @testset "test_compute_kkt_jacobian" begin
