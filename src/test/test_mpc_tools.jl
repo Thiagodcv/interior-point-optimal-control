45e2fc6d1b52120fa5f8c0f7a678c1d8f34d5442
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-11-25 19:49:39 -0800

    Seemed to have solved double integrator

--- src/test/test_mpc_tools.jl
+++ src/test/test_mpc_tools.jl
@@ -225,14 +225,71 @@ end
 end
 
 
-@testset "test_lti_problem" begin
-    """
-    Test to see if can solve an optimal control problem on an LTI system.
-    """
-    n = 3
-    m = 2
+# @testset "test_lti_problem" begin
+#     """
+#     Test to see if can solve an optimal control problem on an LTI system.
+#     """
+#     n = 3
+#     m = 2
 
-    d = [5; 4; 1]
+#     d = [5; 4; 1]
+
+#     # Cost parameters
+#     cost_dict = Dict()
+#     cost_dict["Q"] = Matrix{Float64}(I, n, n)
+#     cost_dict["q"] = -2*d
+#     cost_dict["R"] = 0.01*Matrix{Float64}(I, m, m)
+#     cost_dict["r"] = zeros((m,))
+#     cost_dict["S"] = cost_dict["R"]
+#     cost_dict["Q_T"] = 2*cost_dict["Q"]
+#     cost_dict["q_T"] = 2*cost_dict["q"]
+
+#     # Parameters for variable limits
+#     big_num = 10_000
+#     limit_dict = Dict()
+#     limit_dict["x_ub"] = [4.; 2.; big_num]
+#     limit_dict["x_lb"] = -limit_dict["x_ub"]
+
+#     limit_dict["u_ub"] = [big_num; big_num]
+#     limit_dict["u_lb"] = -limit_dict["u_ub"]
+
+#     limit_dict["du_ub"] = [0.5; 0.5]
+#     limit_dict["du_lb"] = -limit_dict["du_ub"]
+
+#     limit_dict["x_T_ub"] = [4.; 2.; big_num]
+#     limit_dict["x_T_lb"] = -limit_dict["x_T_ub"]
+#     constraint_dict = box_constraints(limit_dict)
+    
+#     # Parameters for LTI system
+#     system_dict = Dict()
+#     system_dict["A"] = Matrix{Float64}(I, n, n)
+#     system_dict["B"] = [1. 0.;
+#                         0. 1.;
+#                         1. 1.]
+#     system_dict["w"] = zeros((n,))
+
+#     # initial state, the latest input, and the time horizon
+#     x0 = [0.; 0.; 0.]
+#     u_latest = [0.; 0.]
+#     T = 10
+
+#     qp_dict = mpc_to_qp(cost_dict, constraint_dict, system_dict, x0, u_latest, T)
+
+#     x_init = zeros((T*(n+m),))
+#     ret = pdip_qp(qp_dict, x_init)
+#     println("iters: ", ret["iters"])
+#     println("solution: ", ret["x"])
+# end
+
+
+@testset "test_double_integrator" begin
+    """
+    Test to see if can solve the double integrator optimal control problem on an LTI system.
+    """
+    n = 2
+    m = 1
+
+    d = [0; 0]
 
     # Cost parameters
     cost_dict = Dict()
@@ -241,37 +298,37 @@ end
     cost_dict["R"] = 0.01*Matrix{Float64}(I, m, m)
     cost_dict["r"] = zeros((m,))
     cost_dict["S"] = cost_dict["R"]
-    cost_dict["Q_T"] = 2*cost_dict["Q"]
-    cost_dict["q_T"] = 2*cost_dict["q"]
+    cost_dict["Q_T"] = cost_dict["Q"]
+    cost_dict["q_T"] = cost_dict["q"]
 
     # Parameters for variable limits
     big_num = 10_000
     limit_dict = Dict()
-    limit_dict["x_ub"] = [4.; 2.; big_num]
+    limit_dict["x_ub"] = [5.; 2.]
     limit_dict["x_lb"] = -limit_dict["x_ub"]
 
-    limit_dict["u_ub"] = [big_num; big_num]
+    limit_dict["u_ub"] = [1.]
     limit_dict["u_lb"] = -limit_dict["u_ub"]
 
-    limit_dict["du_ub"] = [0.5; 0.5]
+    limit_dict["du_ub"] = [0.5]
     limit_dict["du_lb"] = -limit_dict["du_ub"]
 
-    limit_dict["x_T_ub"] = [4.; 2.; big_num]
+    limit_dict["x_T_ub"] = [5.; 2.]
     limit_dict["x_T_lb"] = -limit_dict["x_T_ub"]
     constraint_dict = box_constraints(limit_dict)
     
     # Parameters for LTI system
     system_dict = Dict()
-    system_dict["A"] = Matrix{Float64}(I, n, n)
-    system_dict["B"] = [1. 0.;
-                        0. 1.;
-                        1. 1.]
+    freq = 0.5
+    system_dict["A"] = [1. freq;
+                        0. 1.]
+    system_dict["B"] = [0.; freq]
     system_dict["w"] = zeros((n,))
 
     # initial state, the latest input, and the time horizon
-    x0 = [0.; 0.; 0.]
-    u_latest = [0.; 0.]
-    T = 10
+    x0 = [4.; 1.]
+    u_latest = [0.]
+    T = 20
 
     qp_dict = mpc_to_qp(cost_dict, constraint_dict, system_dict, x0, u_latest, T)
 
