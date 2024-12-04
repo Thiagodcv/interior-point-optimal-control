84a29c697464836703f0386fe531f927011b2c4b
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2024-12-04 12:14:51 -0800

    First implementation of merti_func. Still needs testing

--- src/optimizers/nlp/merit_function.jl
+++ src/optimizers/nlp/merit_function.jl
@@ -1,4 +1,5 @@
 using LinearAlgebra
+include("../../tools/mpc_tools.jl")
 
 
 """
@@ -10,12 +11,19 @@ Evaluate the merit function corresponding to an interior point primal dual metho
 - `z::Array`: the primal variable.
 - `s::Array`: the slack variable.
 - `mu::Float64`: the barrier parameter.
-- `params::Dict{String, Array}`: the parameters of the NLP problem.
+- `params::Dict{String, Array}`: the parameters of the NLP problem. Contains key-value pairs
+        - "H"::Array: the Hessian of the NLP,
+        - "g"::Array: the linear term of the NLP,
+        - "P"::Array: the inequality constraint matrix,
+        - "h"::Array: the inequality constraint vector,
+        - "eq_vec"::Array: the equality constraint residual vector evaluated at z.
 - `rho::Float64`: the parameter of the merit function.
 
 # Returns
-- `Float65`: the merit function evaluated at (z,s) parameterized by (mu, rho).
+- `Float64`: the merit function evaluated at (z,s) parameterized by (mu, rho).
 """
 function merit_func(z, s, mu, params, rho)
-    return -1
+    barrier_obj = transpose(z)*params["H"]*z + transpose(params["g"])*z - mu*sum(log.(s))
+    constraint_cost = rho*norm(params["eq_vec"]) + rho*norm(params["P"]*z - params["h"] + s)
+    return barrier_obj + constraint_cost
 end
